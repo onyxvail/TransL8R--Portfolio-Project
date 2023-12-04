7b14c67d3a9a2cf5808e0675e398e9e44db9f085
onyxvail <ons.hfi@gmail.com>
2023-12-04 18:53:22 +0100

    static fixed

--- transl8r_project/transl8r_project/settings.py
+++ transl8r_project/transl8r_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-0+-&7h4^@tsn_olzv7n1+6o#1h@4#%eicd8sejc6b4byi_98d3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -107,6 +107,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static', 
+)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 #STATIC_ROOT = BASE_DIR / "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -114,4 +119,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Add your Google Cloud API key
-#GOOGLE_TRANSLATE_API_KEY = 'YOUR_GOOGLE_API_KEY'
+GOOGLE_TRANSLATE_API_KEY = 'YOUR_GOOGLE_API_KEY'
